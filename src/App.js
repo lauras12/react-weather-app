3135a16d799449998efcd806907dd4cf2c78fd3a
lauras12 <56985938+lauras12@users.noreply.github.com>
2020-09-23 12:27:12 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import Axios from 'axios';
+import './App.css';
+import DisplayWeather from './components/DisplayWeather.js';
+import Navbar from './components/Navbar.js';
+
+class App extends React.Component {
+
+  //state
+  state = {
+    coords:{
+      latitude: 45,
+      longitude: 60
+    },
+    data: {},
+    inputData: ""
+  }
+
+  componentDidMount() {
+   //get device location
+   if(navigator.geolocation) {
+     
+    navigator.geolocation.getCurrentPosition((position) => {
+      let newCoords = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      }
+
+      this.setState({coords:newCoords});
+
+      //Api call
+      Axios.get(`http://api.weatherstack.com/current?access_key=accb3da1d094eec0ce0cc0e0d895f2a7&query=${this.state.coords.latitude},${this.state.coords.longitude}`).then(res => {
+        let weatherData = {
+          location: res.data.location.name,
+          temperature: res.data.current.temperature,
+          description: res.data.current.weather_descriptions[0],
+          region: res.data.location.region,
+          country: res.data.location.country,
+          wind_speed: res.data.current.wind_speed,
+          pressures: res.data.current.pressure,
+          precip: res.data.current.precip,
+          humidity: res.data.current.humidity,
+          img: res.data.current.weather_icons
+        }
+        this.setState({  data: weatherData  });
+      })
+
+    })
+   }else {
+     console.log("Not Supported")
+   }
+  }
+
+  //Track the input field
+  change = (value) => {
+    this.setState({ inputData:value }) 
+  }
+
+  changeWeather = (event) => {
+    event.preventDefault();
+
+    //api call
+    Axios.get(`http://api.weatherstack.com/current?access_key=accb3da1d094eec0ce0cc0e0d895f2a7&query=${this.state.inputData}`).then(res => {
+      
+    let weatherData = {
+        location: res.data.location.name,
+        temperature: res.data.current.temperature,
+        description: res.data.current.weather_descriptions[0],
+        region: res.data.location.region,
+        country: res.data.location.country,
+        wind_speed: res.data.current.wind_speed,
+        pressures: res.data.current.pressure,
+        precip: res.data.current.precip,
+        humidity: res.data.current.humidity,
+        img: res.data.current.weather_icons
+      }
+      this.setState({  data: weatherData  });
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <Navbar changeWeather = {this.changeWeather} changeRegion = {this.change} />
+        <DisplayWeather weatherData = {this.state.data} />
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
